f7184c4651fd7a5d74fb130d66a30314d579bd4a
Znaidi <znaidi@bilog.fr>
2022-05-19 16:34:26 +0200

    bug2 WS registre docteur

--- src/Entity/Docteur.php
+++ src/Entity/Docteur.php
@@ -6,6 +6,8 @@ use App\Repository\DocteurRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+
 
 /**
  * @ORM\Entity(repositoryClass=DocteurRepository::class)
